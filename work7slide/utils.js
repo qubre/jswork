a0663e0ff955e6645507151134e2eba9f6e09b70
qubre <571012834@qq,com>
2019-11-03 14:43:31 +0800

    work7

--- work7slide/utils.js
+++ work7slide/utils.js
@@ -1,15 +1,46 @@
-function bubbleSort(str){
+function bubbleSort(str) {
     let arr = str.split(',')
     let sortLog = []
-    for(let i = 0;i<arr.length - 1; i++){
-        for(let j=0; j<arr.length - i - 1;j++){
-            sortLog.push([arr.concat(), [j,j + 1 ]])
-            sortLog.push([arr.concat(), [j,j + 1 ]])
-            sortLog.push([arr.concat(), [j,j + 1 ]])
-
-            if( arr [ j ] > arr [ j + 1 ] ){
-                [arr [ j ],arr [ j + 1 ] ] = [ arr [ j + 1 ], arr [ j ] ]
-                sortLog.push([arr.concat(), [j,j + 1 ]])
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - i - 1; j++){
+            sortLog.push([arr.concat(), [j, j + 1]])
+            sortLog.push([arr.concat(), [j, j + 1]])
+            sortLog.push([arr.concat(), [j, j + 1]])
+            if (arr[j] > arr[j + 1]) {
+                [arr[j],arr[j + 1]] = [arr[j + 1], arr[j]]
+                sortLog.push([arr.concat(), [j, j + 1]])
+            }
+        }
+    }
+    sortLog.push([arr.concat(), [-1, -1]])
+    return sortLog
+}
+function showLog(logValues, showElement) {
+    let str = ''
+    let {
+        done,
+        value: [row, pos]
+    } = logValues.next()
+    for (const key in row) {
+        let color = pos.indexOf(Number(key)) > -1 ? 'color:red;' : ''
+        str += '<span style="font-size:' + row[key] * 20 + 'px;' +
+                color + '">' + row[key] + '</span>'
+    }
+    showElement.innerHTML = str
+    if (pos[0] != -1)
+        setTimeout("showLog(logValues,showDiv)", 500)
+}
+function insertSort(str){
+    let arr = str.split(',')
+    let sortLog = []
+    for(let i = 1; i<arr.length ; i++){
+        for(let j=i; j>0 ;j--){
+            sortLog.push([arr.concat(), [j-1,j ]])
+            sortLog.push([arr.concat(), [j-1,j ]])
+            sortLog.push([arr.concat(), [j-1,j ]])
+        if(arr[j-1]>arr[j]){
+                [arr[j-1],arr[j] ]= [arr[j],arr[j-1 ] ]
+                sortLog.push([arr.concat(), [j-1,j ]])
             }
         }
     }
@@ -24,7 +55,6 @@ function showLog(logValues,showElement){
     }=logValues.next()
 
     for (const key in row){
-        //让字体变大
         let color = pos.indexOf(Number(key))> -1 ? 'color:red;':''
         str += '<span style="font-size:' + row[key] * 20 + 'px;' +
             color +'">' + row[key] + '</span>'
@@ -32,47 +62,4 @@ function showLog(logValues,showElement){
             showElement.innerHTML = str
         if(pos[0] != -1)
         setTimeout("showLog(logValues,showDiv)",500)
-    }
-
-
-
-
-
-
-
-
-
-
-    function insertSort(str){
-        let arr = str.split(',')
-        let sortLog = []
-        for(let i = 1; i<arr.length ; i++){
-            for(let j=i; j>0 ;j--){
-                sortLog.push([arr.concat(), [j-1,j ]])
-                sortLog.push([arr.concat(), [j-1,j ]])
-                sortLog.push([arr.concat(), [j-1,j ]])
-            if(arr[j-1]>arr[j]){
-                    [arr[j-1],arr[j] ]= [arr[j],arr[j-1 ] ]
-                    sortLog.push([arr.concat(), [j-1,j ]])
-                }
-            }
-        }
-        sortLog.push([arr.concat(), [ -1,-1 ]])
-        return sortLog
-    }
-    function showLog(logValues,showElement){
-        let str =''
-        let {
-            done,
-            value: [row,pos]
-        }=logValues.next()
-    
-        for (const key in row){
-            let color = pos.indexOf(Number(key))> -1 ? 'color:red;':''
-            str += '<span style="font-size:' + row[key] * 20 + 'px;' +
-                color +'">' + row[key] + '</span>'
-        }
-                showElement.innerHTML = str
-            if(pos[0] != -1)
-            setTimeout("showLog(logValues,showDiv)",500)
     }
